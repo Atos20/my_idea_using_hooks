ed0321755baa549a470d2f4df414047291faa0b4
Orlando Murcio <ato.murcio@gmail.com>
2020-11-25 15:02:50 -0800

    Add 200 status conditional statement to handle errors

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,17 +1,24 @@
 export const getRandomQuote = async () => {
     const promise = await fetch('https://api.tronalddump.io/random/quote')
-    if(promise) {
+    if(promise.status === 200) {
         return promise.json()
     } else {
         return promise.error
     }
 }
 
-
 export const getAllTags= async () => {
     const promise = await fetch('https://api.tronalddump.io/tag')
-    if(promise) {
+    if(promise.status === 200) {
         return promise.json()
+    } else {
+        return promise.error
+    }
+}
+export const getRandomMeme= async () => {
+    const promise = await fetch('https://api.tronalddump.io/random/meme')
+    if(promise.status === 200) {
+        return await promise.blob()
     } else {
         return promise.error
     }
